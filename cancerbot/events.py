5fe1a01243c1481cb93e1355276211febf612882
Sheldon Burks <sheldon.burks@lexmark.com>
2018-10-18 11:55:33 -0400

    Removed print debugging from test events

--- cancerbot/events.py
+++ cancerbot/events.py
@@ -1,11 +1,9 @@
 import logging
 import asyncio
 
-
 from discord import Message, Client, Server
 
 from cancerbot import cancerbot
-
 import cancerbot.schedule as schedule
 
 log = logging.getLogger(__name__)
@@ -14,21 +12,18 @@ log = logging.getLogger(__name__)
 # to look like, this event will not work right now, and it only used for testing/debugging
 @cancerbot.event(cancer_level=1, schedule=schedule.every(10).seconds)
 async def test_event(client: Client, server: Server):
-    log.debug('HERE')
     for channel in server.channels:
         if channel.name == 'general':
             await client.send_message(channel, content='This is a test 1')
 
 @cancerbot.event(cancer_level=1, schedule=schedule.every(30).seconds)
 async def test_event2(client: Client, server: Server):
-    log.debug('HERE2')
     for channel in server.channels:
         if channel.name == 'general':
             await client.send_message(channel, content='This is a test 2')
 
 @cancerbot.event(cancer_level=1, schedule=schedule.every(60).seconds)
 async def test_event3(client: Client, server: Server):
-    log.debug('HERE3')
     for channel in server.channels:
         if channel.name == 'general':
             await client.send_message(channel, content='This is a test 3')
