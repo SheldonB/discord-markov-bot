4a3d97a030d2f1c472c40da2015641dd89e19f76
Sheldon Burks <sheldon.burks@lexmark.com>
2018-10-22 10:46:55 -0400

    Added sing command

--- cancerbot/commands.py
+++ cancerbot/commands.py
@@ -1,3 +1,4 @@
+import asyncio
 from cancerbot import cancerbot
 
 client = cancerbot.get_discord_client()
@@ -13,3 +14,24 @@ async def start():
 @client.command()
 async def stop():
     await client.say('Cancer Bot stopped. See you next time.')
+
+@client.command()
+async def sing():
+    await client.say('Im going to sing you a song :)')
+    await asyncio.sleep(4)
+
+    await client.say('Country Roads')
+    await asyncio.sleep(2)
+    await client.say('Take me home')
+    await asyncio.sleep(2)
+    await client.say('To the place I belong')
+    await asyncio.sleep(2)
+    await client.say('WEST VIRGINIA')
+    await asyncio.sleep(2)
+    await client.say('Mountain Momma')
+    await asyncio.sleep(2)
+    await client.say('Take me home')
+    await asyncio.sleep(2)
+    await client.say('Country Roads')
+
+
