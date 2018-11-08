7ad9bcd68e90ef8f75107c3d0bae50f0a1290c9a
Sheldon Burks <sheldon.burks@lexmark.com>
2018-11-08 09:58:06 -0500

    Removed Uneeded Deps

--- bot.py
+++ bot.py
@@ -2,7 +2,7 @@ import os
 import sys
 import logging
 
-from cancerbot import cancerbot
+from markovbot import markovbot
 
 class CustomFormatter(logging.Formatter):
     LEVEL_MAP = {logging.FATAL: 'F', logging.ERROR: 'E', logging.WARN: 'W', logging.INFO: 'I', logging.DEBUG: 'D'}
@@ -47,4 +47,4 @@ if __name__ == '__main__':
         exit(-1)
 
     log.info('Starting Discord Cancer Bot')
-    cancerbot.run(token)
+    markovbot.run(token)
