4e010344de02950e5d2616208ca7562e221b4c67
Sheldon Burks <sheldon.burks@lexmark.com>
2020-05-19 11:57:23 -0400

    Fix Async Tests

--- test/test_supervisor.py
+++ test/test_supervisor.py
@@ -1,4 +1,8 @@
 import unittest
+from unittest.mock import Mock
+
+import asyncio
+from asyncio import coroutine
 
 from markovbot.supervisor import Supervisor
 from .utils import get_guild
@@ -7,17 +11,23 @@ from .utils import get_guild
 class SupervisorTest(unittest.TestCase):
     def test_can_add_guild(self):
         supervisor = Supervisor()
+        supervisor.seeder.seed = Mock(side_effect=coroutine(Mock()))
+
         guild = get_guild()
 
-        supervisor.add(guild)
+        loop = asyncio.get_event_loop()
+        loop.run_until_complete(supervisor.add(guild))
 
         self.assertIn(guild.id, supervisor.guilds)
 
     def test_can_remove_guild(self):
         supervisor = Supervisor()
+        supervisor.seeder.seed = Mock(side_effect=coroutine(Mock()))
         guild = get_guild()
 
-        supervisor.add(guild)
+        loop = asyncio.get_event_loop()
+        loop.run_until_complete(supervisor.add(guild))
+
         supervisor.remove(guild)
 
         self.assertNotIn(guild.id, supervisor.guilds)
